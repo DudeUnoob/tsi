e7fb5b34764bba6af4277a38bfddbf902a1b96dc
Avanish <avanishsamala@gmail.com>
2026-07-14 23:42:14 -0400

    Whitelist firebasestorage.googleapis.com and *.squarespace.com domains in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -8,6 +8,22 @@ const nextConfig: NextConfig = {
         hostname: 'images.squarespace-cdn.com',
       },
       {
+        protocol: 'http',
+        hostname: 'static1.squarespace.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'static1.squarespace.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'firebasestorage.googleapis.com',
+      },
+      {
+        protocol: 'https',
+        hostname: '**.squarespace.com',
+      },
+      {
         protocol: 'https',
         hostname: '*.supabase.co',
       },
